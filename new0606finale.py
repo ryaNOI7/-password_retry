6a25ce56459f83da6ace5dd785c4fd158898ba0c
ryaNOI7 <zhongyirniu@gmail.com>
2022-06-06 17:14:59 +0800

    change th while i > 0

---
+++ new0606finale.py
@@ -0,0 +1,10 @@
+x = 3
+password = 'a123456'
+while x > 0: 
+    pwd = input('請輸入密碼: ')
+    if pwd == password: 
+        print("登入成功! ")
+        break
+    else: 
+        x = x - 1   
+        print('密碼錯誤! 還有', x, '次機會')
